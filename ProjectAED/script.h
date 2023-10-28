0b912805c3aafc52de55eacc3ed50459e530ea4d
Bruno <up202205619@up.pt>
2023-10-28 22:30:50 +0100

    Added doxygen comments to script.h

--- ProjectAED/script.h
+++ ProjectAED/script.h
@@ -20,15 +20,40 @@
  * This is part 2.
  */
 
+/**
+ * \class Script
+ * \brief This class handles and runs the different commands.
+ *
+ * This class has the course LEIC and different functions to handle alterations to that data.
+ * It is also responsible for handling inputs and outputs.
+ */
 class Script {
 private:
     LEIC data;
 public:
+    /**
+     * \brief Creates a new Script.
+     */
     Script();
+    /**
+     * \brief Outputs the main menu and handles the respective inputs.
+     */
     void run();
+    /**
+     * \brief Outputs the request menu and handles the respective inputs.
+     */
     void request();
+    /**
+     * \brief Outputs the listings menu and handles the respective inputs.
+     */
     void listings();
+    /**
+     * \brief Outputs the schedule listing menu and handles the respective inputs.
+     */
     void listSchedules();
+    /**
+     * \brief Outputs the Student listing menu and handles the respective inputs.
+     */
     void listStudents();
 };
 
