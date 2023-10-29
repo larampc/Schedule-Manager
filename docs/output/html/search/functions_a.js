a78f83d9b0f02981cf25dcf5df6ebcb3df9ae521
Bruno <up202205619@up.pt>
2023-10-29 22:11:31 +0000

    Added doxygen comments in script.h

--- docs/output/html/search/functions_a.js
+++ docs/output/html/search/functions_a.js
@@ -1,4 +1,8 @@
 var searchData=
 [
-  ['time_0',['time',['../class_time.html#a15f171401599a9a058e89e6cf3244f2b',1,'Time::Time(std::string time)'],['../class_time.html#a6a5659e8c5ba951a1a2711be2184eba6',1,'Time::Time(float time)']]]
+  ['save_5fto_5ffiles_0',['save_to_files',['../class_l_e_i_c.html#a14416642527a2f02929b50b0735cb5e8',1,'LEIC']]],
+  ['script_1',['Script',['../class_script.html#abfe4bfbb5bfc35131f16531818354349',1,'Script']]],
+  ['set_5fcap_2',['set_cap',['../class_l_e_i_c.html#a5109feb2b28aa7c33c2ec151b51cc635',1,'LEIC']]],
+  ['set_5ftype_3',['set_type',['../class_request.html#a48bee5c53e5d88620e247f15bd22b86f',1,'Request']]],
+  ['student_4',['Student',['../class_student.html#a20ea4b5d4a87c05ba2b4d95ffd475776',1,'Student']]]
 ];
