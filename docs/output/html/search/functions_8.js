e4a1b8ab2948933d5afe3fc74670de87cc4fd8aa
Bruno <up202205619@up.pt>
2023-10-29 10:04:38 +0000

    Added doxygen comments to request.h

--- docs/output/html/search/functions_8.js
+++ docs/output/html/search/functions_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['script_0',['Script',['../class_script.html#a10975b78b03162488c522f095fd7707f',1,'Script']]],
-  ['student_1',['Student',['../class_student.html#a20ea4b5d4a87c05ba2b4d95ffd475776',1,'Student']]]
+  ['set_5ftype_1',['set_type',['../class_request.html#a48bee5c53e5d88620e247f15bd22b86f',1,'Request']]],
+  ['student_2',['Student',['../class_student.html#a20ea4b5d4a87c05ba2b4d95ffd475776',1,'Student']]]
 ];
