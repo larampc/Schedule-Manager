061336dad5eaf8f5b8fc5c8fe7aa7cff28e3694c
Bruno <up202205619@up.pt>
2023-10-30 18:07:37 +0000

    Fixed bug with colors

--- ProjectAED/color_print.cpp
+++ ProjectAED/color_print.cpp
@@ -45,24 +45,26 @@ Color_Print::Color_Print(bool use_colors, std::string color, std::string line, b
                          std::string background_color) {
         std::string col = "\033[0";
         std::string back_col = "m";
+        if (use_colors)
+        {
+            if (color == "gray") col = "\033[0;30";
+            else if (color == "blue") col = "\033[0;34";
+            else if (color == "green") col = "\033[0;32";
+            else if (color == "cyan") col = "\033[0;36";
+            else if (color == "red") col = "\033[0;31";
+            else if (color == "pink") col = "\033[0;35";
+            else if (color == "yellow") col = "\033[0;33";
+            else if (color == "white") col = "\033[0;37";
 
-        if (color == "gray") col = "\033[0;30";
-        else if (color == "blue") col = "\033[0;34";
-        else if (color == "green") col = "\033[0;32";
-        else if (color == "cyan") col = "\033[0;36";
-        else if (color == "red") col = "\033[0;31";
-        else if (color == "pink") col = "\033[0;35";
-        else if (color == "yellow") col = "\033[0;33";
-        else if (color == "white") col = "\033[0;37";
-
-        if (background_color == "none") back_col = "m";
-        else if (background_color == "blue") back_col = ";44m";
-        else if (background_color == "green") back_col = ";42m";
-        else if (background_color == "cyan") back_col = ";46m";
-        else if (background_color == "red") back_col = ";41m";
-        else if (background_color == "pink") back_col = ";45m";
-        else if (background_color == "yellow") back_col = ";43m";
-        else if (background_color == "white") back_col = ";47m";
+            if (background_color == "none") back_col = "m";
+            else if (background_color == "blue") back_col = ";44m";
+            else if (background_color == "green") back_col = ";42m";
+            else if (background_color == "cyan") back_col = ";46m";
+            else if (background_color == "red") back_col = ";41m";
+            else if (background_color == "pink") back_col = ";45m";
+            else if (background_color == "yellow") back_col = ";43m";
+            else if (background_color == "white") back_col = ";47m";
+        }
 
         std::cout << col + back_col  << line << "\033[0m";
         if (newLine) {
