d68012c1cd83327895e0afdd6ab186be623a0aad
Bruno <up202205619@up.pt>
2023-10-31 10:28:41 +0000

    Added doxygen comments to color_print.h

---
+++ docs/output/html/search/functions_d.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['uc_5fhas_5fvacancy_0',['Uc_has_vacancy',['../class_l_e_i_c.html#a5a03dcd18c4066822f1839c9a9f65055',1,'LEIC']]],
+  ['undo_5frequest_1',['undo_request',['../class_l_e_i_c.html#acbc8c03673710feb402bf1ef9808af33',1,'LEIC']]],
+  ['upload_5frequests_2',['upload_requests',['../class_l_e_i_c.html#a2d38717562cf97b2976516ea4c813dbf',1,'LEIC']]]
+];
